c7edb85d82e300bd7d3020d73fb1e16b86574d85
Lucian Snare <lsnare1@gmail.com>
2015-09-07 13:59:10 -0400

    Working on new index page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,14 +26,21 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    //get("/hello", (req, res) -> "Hello World");
 
-    get("/", (request, response) -> {
+    /*get("/", (request, response) -> {
       Map<String, Object> attributes = new HashMap<>();
       attributes.put("message", "Hello World!");
 
-      return new ModelAndView(attributes, "index.ftl");
-    }, new FreeMarkerEngine());
+      return new ModelAndView(attributes, "_index.ftl");
+    }, new FreeMarkerEngine());*/
+
+      get("/", (request, response) -> {
+          Map<String, Object> attributes = new HashMap<>();
+          attributes.put("message", "Hello World!");
+
+          return new ModelAndView(attributes, "index.ftl");
+      }, new FreeMarkerEngine());
 
     get("/film", (request, response) -> {
       Map<String, Object> attributes = new HashMap<>();
