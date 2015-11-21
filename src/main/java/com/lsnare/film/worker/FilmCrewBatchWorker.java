65f9c8fa62647a0bc2525b3b564eb9940bcd0d55
Lucian Snare <lds@trifecta.com>
2015-11-20 22:17:38 -0500

    Creating batch job to update films with actors, directors, etc. in the background

---
+++ src/main/java/com/lsnare/film/worker/FilmCrewBatchWorker.java
@@ -0,0 +1,27 @@
+package com.lsnare.film.worker;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by lucian.snare on 11/20/2015.
+ */
+public class FilmCrewBatchWorker {
+    public static final int RUN_INTERVAL = 30000;
+    static Log log = LogFactory.getLog(FilmCrewBatchWorker.class);
+
+    public static void main(String[] args) {
+        try {
+            //check film records that need to be updated
+            log.info("Processing film updates...");
+            //update all dirty records
+
+            //sleep
+            log.info("Film updates complete");
+            Thread.sleep(RUN_INTERVAL);
+        } catch (Exception e) {
+
+        }
+    }
+
+}
