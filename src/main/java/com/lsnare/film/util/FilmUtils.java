5f9d0bb3d5fa58acaa3072a681bd23f8983f99c7
Lucian Snare <lds@trifecta.com>
2016-02-20 23:20:15 -0500

    Fixing filmData string to handle films without a listed director

--- src/main/java/com/lsnare/film/util/FilmUtils.java
+++ src/main/java/com/lsnare/film/util/FilmUtils.java
@@ -144,10 +144,11 @@ public class FilmUtils {
             //Create a list of radio inputs with values set to each film's IMDB Id
             for (Movie movie : movies){
                 filmData += "<input type=\"radio\" name=\"film\" value=\"" + movie.getIdIMDB() + "\">"
-                        + "<i>" + movie.getTitle() + "</i>"
-                            + movie.getDirectors() != null ? ", directed by " + movie.getDirectors().get(0).getName()
-                            : ""
-                            + " in " + movie.getYear() + "<br>";
+                        + "<i>" + movie.getTitle() + "</i>(" + movie.getYear() + ")";
+                if (movie.getDirectors().size() > 0){
+                    filmData += ", directed by " + movie.getDirectors().get(0).getName();
+                }
+                filmData += "<br>";
             }
             filmData += "<input type=\"submit\" name=\"insertFilm\" value=\"Insert Film\" form=\"searchResultsTable\"/>";
             filmData += "</fieldset>";
